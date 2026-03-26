a4ff3c1b69f77467873da5fd54c8ef94626aa8ef
Savanyv <mrsaddam2806@gmail.com>
2026-03-26 16:17:38 +0700

    chore: adding logger for gorm

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -3,11 +3,14 @@ package database
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/savanyv/zenith-pay/config"
 	"github.com/savanyv/zenith-pay/internal/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
@@ -22,7 +25,20 @@ func InitDatabase(cfg *config.Config) (*gorm.DB, error) {
 		cfg.DBName,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel: logger.Error,
+			IgnoreRecordNotFoundError: true,
+			Colorful: true,
+		},
+	)
+
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		return nil, err
 	}
